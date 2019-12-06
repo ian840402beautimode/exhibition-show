3361a338b12259703dc85ba622fd3d4be485ef01
Ian.Zheng <ian.zheng840402@gmail.com>
2019-12-06 13:38:13 +0800

    專案初始化

--- src/js/controller/common/return-is-image-title.js
+++
@@ -1,8 +0,0 @@
-//判斷是否有圖片title
-export let isImageTitle = (imageTitle ,loadImageInfo) => {
-    if(imageTitle == "" || imageTitle == null){
-        //do nothing
-    }else{
-        loadImageInfo();
-    }
-}
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,3 @@
-import './pluging';
-import './page_view.js';
-import './controller/form/login.js';
-
-
-
 // 讀取為使用到的圖片
-var requireContext = require.context("../images", true, /^\.\/.*\.png$|^\.\/.*\.jp?g$|/);
-requireContext.keys().map(requireContext);
+// var requireContext = require.context("../images", true, /^\.\/.*\.png$|^\.\/.*\.jp?g$|/);
+// requireContext.keys().map(requireContext);
