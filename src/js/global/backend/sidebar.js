8379c58de2eeaa17a8b971a96dce906bf00989aa
杜承達 <jeremy@bsdctw.com>
2020-03-13 17:45:02 +0800

    修改檔案/前端: js fix

--- src/js/global/backend/sidebar.js
+++ src/js/global/backend/sidebar.js
@@ -1,3 +1,18 @@
+const position = location.pathname;
+const path = location.pathname.substring(1).split('/')[1];
+
+// 預設打開的sidebar 
+if(path !== 'dashboard'){
+    $('#admin-sidebar-block').find(".menu__link").each(function(){
+        let item = new RegExp($(this).attr("href"));
+        if (item.test(position)) {
+            $(this).parents(".menu-list__item").addClass('_active');
+            $(this).parents(".sub-menu__item").addClass('_active');
+            $(this).parents(".sub-menu").slideDown(0);
+        }
+    });
+}
+
 $("#admin-sidebar-btn").on("click",function() {
     $("#admin-sidebar").toggleClass("_hidden");
     $('main').toggleClass('_scroll');
@@ -13,33 +28,4 @@ $("#admin-sidebar-block").find(".menu-list__item__title").on("click",function(){
         $(this).next().slideDown();
         $(this).parent().addClass('_active');
     }  
-})
-
-
-//TODO:active連結想其他方法做
-export let sidebarPosition = idName => {
-    let position = location.pathname;
-    idName.find(".menu__link").each(function(){
-        let item = new RegExp($(this).attr("href"));
-        if (item.test(position)) {
-            $(this).parents(".menu-list__item").addClass('_active');
-            $(this).parents(".sub-menu__item").addClass('_active');
-            $(this).parents(".sub-menu").slideDown(0);
-        }
-    });
-}
-
-//判斷 現在網址是否是 後台首頁
-let id = location.pathname.substring(1).split('/')[1];
-if(id !== 'main'){
-    sidebarPosition($("#admin-sidebar-block"));
-}
-
-// 清除 sessionStorage 
-$('.sub-menu__item').on('click', function(){
-    sessionStorage.removeItem('tablePageNum');
-    // 清除有層級列表的值
-    sessionStorage.removeItem('first-name');
-    sessionStorage.removeItem('second-name');
-    
 })
