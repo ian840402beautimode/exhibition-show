d16e5d5ce3e9f95580c1a16b9fe96f68f56556d8
Ian.Zheng <ian.zheng840402@gmail.com>
2020-07-23 18:26:47 +0800

    新增頁面

---
+++ src/js/main-menu.js
@@ -0,0 +1,4 @@
+$('#menu-btn').on('click', function (e) {
+  $('.menu-wrap').slideToggle()
+  $('.menu-mask').toggleClass('open')
+})
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,4 @@
+import './main-menu'
 import './exhibition-area'
+
+// window.addEventListener("wheel", event => console.info(event.deltaY));
