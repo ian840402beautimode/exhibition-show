e883fbc6c526a79c00e84ab7183546c1323a950c
Ian.Zheng <ian.zheng840402@gmail.com>
2020-07-30 13:57:55 +0800

    把RWD打開

--- src/js/3d-translate.js
+++ src/js/3d-translate.js
@@ -1,19 +1,24 @@
 document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
 const rotateDegree = 10
+const offsetDistance = 50
 const windowHalfX = window.innerWidth / 2
 const windowHalfY = window.innerHeight / 2
 const mouseDegreeX = windowHalfX / rotateDegree
 const mouseDegreeY = windowHalfY / rotateDegree
+const mouseOffsetX = windowHalfX / offsetDistance
+const mouseOffsetY = windowHalfY / (offsetDistance / 2 )
 
 // 滑鼠移動事件
 function onDocumentMouseMove( event ) {
   const mouseX = event.clientX - windowHalfX
-  const mousey = event.clientY - windowHalfY
+  const mouseY = event.clientY - windowHalfY
+  const offsetX = mouseX / mouseOffsetX
+  const offsetY = mouseY / mouseOffsetY
   const degreeX = mouseX / mouseDegreeX
-  const degreeY = mousey / mouseDegreeY
+  const degreeY = mouseY / mouseDegreeY
 
   $('#main-title').css({
-    'transform': `perspective(1000px) rotateY(${degreeX}deg) rotateX(${-degreeY}deg)`
+    'transform': `perspective(1000px) rotateY(${degreeX}deg) rotateX(${-degreeY}deg) translate(${offsetX}px, ${-offsetY}px)`
   })
 }
